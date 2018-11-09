bad7ab8cefb3e500dd9b79ebeea86acddfbce250
ruanwang <fas>
2018-11-09 11:44:09 +0800

    dfsfs

---
+++ src/main/java/Pa.java
@@ -0,0 +1,2 @@
+public class Pa {
+}
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,5 +3,6 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 public class User {
     public static void main(String[] args) {
         System.out.println(23);
+        System.out.println(22);
     }
 }
